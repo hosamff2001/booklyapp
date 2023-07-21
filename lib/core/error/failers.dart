ac35ed03a054bafaad65cb1fc8b0dbda3f490204
hosamff2001 <hosamff2001@gmail.com>
2023-07-21 17:36:27 +0300

    add dartz and failer class

---
+++ lib/core/error/failers.dart
@@ -0,0 +1,3 @@
+abstract class Failler {}
+
+class ServerFailer extends Failler {}
--- lib/features/home/data/rebos/homerepos.dart
+++ lib/features/home/data/rebos/homerepos.dart
@@ -1,4 +1,7 @@
+import 'package:booklyapp/core/error/failers.dart';
+import 'package:booklyapp/features/home/data/model/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
 abstract class HomeRepos {
-  featchbestsellerbooks();
-  featchfeaturesbooks();
+  Future <Either<Failler,List<BookModel>>> featchbestsellerbooks();
+  Future <Either<Failler,List<BookModel>>> featchfeaturesbooks();
 }
